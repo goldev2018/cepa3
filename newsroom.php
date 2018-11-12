6361b039002a480933244997aafefce72822bebd
carlo del valle <delvallecarlo@gmail.com>
2018-11-12 12:00:39 +0800

    nov3,2018 revision

--- newsroom.php
+++ newsroom.php
@@ -19,61 +19,70 @@
 
 
 
+
+
+
+
+
+
+
+
 <section id="content">
-	<div class="container">
-		<div class="row" >
-			<div class="col-lg-12">
-			<h3>NEWSROOM</h3>
-			<div class="solidline"></div>
+			<div class="container animated fadeIn">
+							 <div class="row">
+								 <div class="col-lg-12">
+								 <div class="col-sm-8 col-lg-8">
+									 <h3>NEWSROOM</h3>
+									 <div class="solidline"></div>
+								 	<div class="row"><!-- CONTENT --></div>
 
+								 </div>
 
-			<!-- Modal -->
-			<div id="myModal" class="modal" data-keyboard="false" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class="modal-dialog modal-dialog-centered" role="document">
-			  <div class="modal-content">
-			      <div class="modal-header">
-							<!-- <a href="index.php"><button type="button" class="close">&times;</button></a> -->
-			          <h4 class="modal-title">Disclaimer</h4>
-			      </div>
-			      <div class="modal-body"><p><strong>THIS SECTION OF THE WEBSITE CONTAINS ANNOUNCEMENTS, DOCUMENTS AND INFORMATION (THE “INFORMATION”) PUBLISHED BY STROBE RELATING TO THE [POSSIBLE] OFFER IN COMPLIANCE WITH THE CITY CODE ON TAKEOVERS AND MERGERS (THE “CODE”). THE OFFER CANNOT BE VALIDLY ACCEPTED BY BEAM SHAREHOLDERS OR ANY OTHER PERSONS BY MEANS OF DOWNLOADING A COPY OF THE OFFER DOCUMENTS FROM THIS WEBSITE.
-			</strong></p>
-			<p><strong>THE INFORMATION IS BEING MADE AVAILABLE IN GOOD FAITH AND FOR INFORMATION PURPOSES ONLY, AND ITS AVAILABILITY IS SUBJECT TO THE TERMS AND CONDITIONS SET OUT BELOW.  IF YOU ARE NOT PERMITTED TO VIEW THE INFORMATION ON THIS WEBSITE, OR VIEWING THE INFORMATION WOULD RESULT IN A BREACH OF THOSE TERMS AND CONDITIONS, OR YOU ARE IN ANY DOUBT AS TO WHETHER YOU ARE PERMITTED TO VIEW THE INFORMATION, PLEASE EXIT THIS WEB PAGE.</strong></p>
+									<div class="col-sm-4">
+										<h3>ANNOUNCEMENTS</h3>
+										<div class="solidline"></div>
+										<div class="col-xs-11 col-sm-10 col-md-10 col-lg-10">
+											<a href="announcements.php"><p>Click here for future announcements</p></a>
+										</div>
+									</div>
 
-			        <p><strong>Basis of access</strong></p>
-			        <p>Please read this notice carefully - it applies to all persons who view this part of the website and, depending upon who you are and where you live, it may affect your rights. This notice and the information contained herein may be altered or updated from time to time, and should be read carefully each time you visit this part of the website.</p>
-			        <p>Strobe reserves the right to amend or update this notice at any time and you should, therefore, read it in full each time you visit the website. In addition, the contents of the website may be amended at any time in whole or in part at the sole discretion of Strobe.</p>
-			        <p>For regulatory reasons, we have to ensure you are aware of the appropriate regulations for the country which you are in.  To allow you to view the Information, you have to read the following notices.</p>
+									<div class="col-sm-4" style="float: right">
+										<h3>CONTACT US</h3>
+										<div class="solidline"></div>
+										<Address>
+ 				 							<!-- <i class="glyphicon glyphicon-home"></i> -->
+											<div class="col-xs-1 col-sm-2 col-md-2 col-lg-2">
+ 				 							<i class="glyphicon glyphicon-map-marker accent"></i>
+											</div>
+											<div class="col-xs-11 col-sm-10 col-md-10 col-lg-10">
 
-			        <p><strong>Overseas persons</strong></p>
-			        <p>The information contained herein is not for publication or distribution, directly or indirectly, in or into any jurisdiction where to do so would violate the law of that jurisdiction (a “Restricted Jurisdiction”), and the availability of the Offer to shareholders who are not resident in the United Kingdom may be affected by the laws of relevant jurisdictions.  Persons who are not resident in the United Kingdom should inform themselves of, and observe, any applicable regulatory and legal requirements.</p>
-			        <p>Unless otherwise determined by Strobe or required by the Code, and permitted by applicable law and regulation, the Offer will not be made available, directly or indirectly, in or into a Restricted Jurisdiction where to do so would violate the securities laws in that jurisdiction and the Offer is not capable of acceptance from or within a Restricted Jurisdiction except pursuant to an applicable exemption.  The Offer may not be downloaded or accessed by any person either in whole or in part from or within a Restricted Jurisdiction or where to do so would or may constitute a breach of any applicable local laws or regulations.</p>
+												Registered Address – <br>
+												Hazelwood Gregory Lane,<br>
+												Durley, Southampton,<br>
+												United Kingdom, SO32 2BS<br>
+												<br>
+												Hong Kong Address - <br>
+												Suite 4823, 48th Floor,<br>
+												Sun Hung Kai Centre, 30 Harbour Road,<br>
+												Hong Kong<br><br>
 
-			        <p><strong>IF YOU ARE NOT PERMITTED TO VIEW THE INFORMATION ON THIS WEBSITE, OR VIEWING THE INFORMATION WOULD RESULT IN A BREACH OF THE ABOVE, OR YOU ARE IN ANY DOUBT AS TO WHETHER YOU ARE PERMITTED TO VIEW THE INFORMATION, PLEASE EXIT THIS WEB PAGE.</strong></p>
+											</div>
 
-			        <p><strong>Forward-looking statements</strong></p>
-			        <p>The information and documents in this section of the website may contain forward-looking statements regarding Strobe's or Beam's financial position, business strategy, plans and objectives of management for future operations.  All statements other than statements of historical facts included in any document may be forward-looking statements.  Forward-looking statements also often use words such as “targets”, “plans”, “believes”, “expects”, “aims”, “intends”, “will”, “may”, “anticipates”, “estimates”, “projects”, “should” and words of similar meaning.  By their nature, forward-looking statements involve risk and uncertainty because they relate to future events and circumstances, and readers are cautioned not to place undue reliance on such statements.  The important factors that could cause Beam's and/or Strobe's actual results, performance or achievements to differ materially from those in the forward-looking statements include, among others: the performance of competitors; the impact of legal or other proceedings; and general market and/or economic conditions. </p>
+											<div class="col-xs-1 col-sm-2 col-md-2 col-lg-2">
+ 				 							<i class="glyphicon glyphicon-earphone accent"></i>
+											</div>
+											<div class="col-xs-11 col-sm-10 col-md-10 col-lg-10">
+ 				 							(852) 2861 0719
+											</div>
 
-			        <p>These forward-looking statements speak only as of the date of the relevant document(s).  Strobe expressly disclaims any obligation or undertaking to update or revise any forward-looking statement (except to the extent legally required).</p>
+											<!-- <p><i class="glyphicon glyphicon-earphone"></i>&nbsp;(+632) 831 8855 </p> -->
+										</address>
+									</div>
 
-			        <p>Unless expressly stated otherwise, no statement contained or referred to in this section of the website is intended to be a profit forecast.</p>
-
-			        <p><strong>Confirmation of understanding and acceptance of disclaimer</strong></p>
-
-			        <p>I have read and understood the disclaimer and terms and conditions set out above.  I understand that it may affect my rights.  I agree to be bound by its terms.  I confirm that I am permitted to proceed to this part of the site.</p></div>
-			      <div class="modal-footer">
-							<a href="index.php"><button type="button" class="btn btn-secondary">< Back</button></a>
-			        <button type="button" class="btn btn-default" data-dismiss="modal">Agree</button>
-
-			      </div>
-			  </div>
+								</div>
+					</div>
 			</div>
-			</div>
-
-
-			</div>
-		</div>
-	</div>
-</section>
+		</section>
 
 
 </body>
